bfc23b44b82ab2562b0ed219bb6c06062db919aa
Hui Chun Ong <suer.ong@gmail.com>
2024-06-03 17:06:26 +0800

    Disable vi-mode in tmux.conf. Fix neovim plugin manager install bug. Update README.md

--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-sudo apt install neovim tmux git -y
+sudo apt install curl git neovim tmux -y
 
 # Download neovim config: init.vim
 sh -c 'curl -fLo $HOME/.config/nvim/init.vim --create-dirs \
 	     https://raw.githubusercontent.com/huichuno/vimrc/main/init.vim'
 
 # Install neovim plugin manager
-sh -c 'curl -fLo "$HOME/.local/share}"/nvim/site/autoload/plug.vim \
-       --create-dirs \
+sh -c 'curl -fLo "$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Download tmux config: tmux.conf
@@ -24,6 +23,6 @@ echo
 echo 'Launch "nvim" > press <Enter> > run ":PlugInstall" command'
 echo 'Run "PlugUpdate" to install or update plugins'
 echo
-echo 'Launch "tmux" > press "Ctrl + Space + I" to install plugin'
+echo 'Launch "tmux" > press "Ctrl + b + I" to install plugin'
 echo
 
